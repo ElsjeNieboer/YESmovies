4dce2c98a6094ff13a97ae7ce0e89ce27043066a
unknown <joleinvandersluis@gmail.com>
2019-04-23 15:13:13 +0200

    in Class Movie: set basic fields and generated setters/getters

---
+++ src/nl/YESmovies/Movie.java
@@ -0,0 +1,38 @@
+package nl.YESmovies;
+
+import java.util.ArrayList;
+
+public class Movie {
+    private long id;
+    private String title;
+    private short releaseYear;
+    private ArrayList<String> genres;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public short getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(short releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(ArrayList<String> genres) {
+        this.genres = genres;
+    }
+}
