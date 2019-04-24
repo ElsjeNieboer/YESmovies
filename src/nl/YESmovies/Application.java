b9c43981f06753a3f1e7ae74b2c579c8bb5c73c9
unknown <joleinvandersluis@gmail.com>
2019-04-24 16:32:31 +0200

    unfinished setup of the addRating option

--- src/nl/YESmovies/Application.java
+++ src/nl/YESmovies/Application.java
@@ -23,6 +23,72 @@ public class Application {
                 // Add Movie
             } else if (userInput.equals("r")) {
                 // Add Rating
+
+
+
+
+
+
+                do {
+                    System.out.println("To rate a movie, please enter your username. To return to main screen, press c");
+
+                    String userName = reader.nextLine().trim();
+                    /*if (userName == "c"){
+                    continue MAIN_LOOP;
+                    }
+                    else */
+                    if (Profile.profileList.contains(userName)) {
+                        do{
+                            System.out.println("You are now rating as user: " + userName + "\nWhat movie would you like to rate?, please enter the full movie title. To return to main screen press c");
+                            String movieTitle = reader.nextLine().trim();
+                            /*if (movieTitle == "c"){
+                            continue MAIN_LOOP;
+                            }
+                            else */
+                            if(Movie.movieList.contains(movieTitle)){
+                                do {
+                                    System.out.println("You are rating " + movieTitle + "\nHow would you rate this movie? Enter a rating between 0.0 and 10.0. To return to main screen press c");
+
+                                    double movieRating = Double.parseDouble(reader.nextLine());
+                                    /*if (movieTitle == "c"){
+                                    continue MAIN_LOOP;
+                                    }
+                                    else */
+                                    if (movieRating >= 0 & movieRating <= 10) {
+                                        movieRating = (double) Math.round(movieRating * 10) / 10;
+                                    } else {
+                                        System.out.println("that is not a valid rating, please try again");
+                                        continue;
+                                    }
+                                }
+                                while(true);
+
+                            } else{
+                                System.out.println("Movie not found, please try again.");
+                                continue;
+                            }
+                        }while(true);
+
+                    } else {
+                        System.out.println("Username not found, please try again.");
+                        continue;
+                    }
+
+                }while(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             } else if (userInput.equals("q")){
                 keepGoing = false;
             } else {
---
+++ src/nl/YESmovies/TestApplication.java
@@ -0,0 +1,18 @@
+package nl.YESmovies;
+
+import java.util.Scanner;
+
+public class TestApplication {
+    public static void main(String[] args) {
+
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("To rate a movie, please enter your username. To cancel, press c");
+            String userName = scan.nextLine();
+            if userName = c
+                    
+
+        }
+    }
+}
