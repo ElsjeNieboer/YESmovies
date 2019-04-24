6a5104b7a93425a0adb38d578bb619c7762c6531
unknown <joleinvandersluis@gmail.com>
2019-04-24 16:39:29 +0200

    added continue to MENU_LOOP

--- src/nl/YESmovies/Application.java
+++ src/nl/YESmovies/Application.java
@@ -33,28 +33,22 @@ public class Application {
                     System.out.println("To rate a movie, please enter your username. To return to main screen, press c");
 
                     String userName = reader.nextLine().trim();
-                    /*if (userName == "c"){
-                    continue MAIN_LOOP;
-                    }
-                    else */
-                    if (Profile.profileList.contains(userName)) {
+                    if (userName == "c"){
+                        continue MENU_OPTIONS;
+                    } else if (Profile.profileList.contains(userName)) {
                         do{
                             System.out.println("You are now rating as user: " + userName + "\nWhat movie would you like to rate?, please enter the full movie title. To return to main screen press c");
                             String movieTitle = reader.nextLine().trim();
-                            /*if (movieTitle == "c"){
-                            continue MAIN_LOOP;
-                            }
-                            else */
-                            if(Movie.movieList.contains(movieTitle)){
+                            if (movieTitle == "c"){
+                                continue MENU_OPTIONS;
+                            } else if(Movie.movieList.contains(movieTitle)){
                                 do {
                                     System.out.println("You are rating " + movieTitle + "\nHow would you rate this movie? Enter a rating between 0.0 and 10.0. To return to main screen press c");
 
                                     double movieRating = Double.parseDouble(reader.nextLine());
-                                    /*if (movieTitle == "c"){
-                                    continue MAIN_LOOP;
-                                    }
-                                    else */
-                                    if (movieRating >= 0 & movieRating <= 10) {
+                                    if (movieTitle == "c"){
+                                        continue MENU_OPTIONS;
+                                    } else if (movieRating >= 0 & movieRating <= 10) {
                                         movieRating = (double) Math.round(movieRating * 10) / 10;
                                     } else {
                                         System.out.println("that is not a valid rating, please try again");
