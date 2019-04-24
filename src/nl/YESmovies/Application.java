0d3bfecd238013f5000462999869b4f91c51b610
elsjen <elsje_nieboer_1@hotmail.com>
2019-04-24 15:30:50 +0200

    Add Application main

--- src/nl/YESmovies/Application.java
+++ src/nl/YESmovies/Application.java
@@ -6,23 +6,29 @@ import java.util.ArrayList;
 public class Application {
     public static void main(String[] args) {
         // Scanner
-        Scanner reader = new Scanner(System.in);
 
-        System.out.println("If you want to add a new profile, press 'p'. If you want to add a new movie, press 'm'." +
-                "If you want to add a new rating, press r.");
+        boolean keepGoing = true;
 
-        String userInput = reader.nextLine();
+        do {
+            Scanner reader = new Scanner(System.in);
 
-        if(userInput.equals("p")){
-            // Add Profile
-        } else if(userInput.equals("m")){
-            // Add Movie
-        } else if(userInput.equals("r")){
-            // Add Rating
-        } else {
-            System.out.println("Please enter a valid option. If you want to add a new profile, press 'p'. If you" +
-                    " want to add a new movie, press 'm'.If you want to add a new rating, press r.");
-        }
+            System.out.println("If you want to add a new profile, press 'p'. If you want to add a new movie, press 'm'." +
+                    "If you want to add a new rating, press r. If you want to quit, press 'q'.");
+
+            String userInput = reader.nextLine();
+
+            if (userInput.equals("p")) {
+                // Add Profile
+            } else if (userInput.equals("m")) {
+                // Add Movie
+            } else if (userInput.equals("r")) {
+                // Add Rating
+            } else if (userInput.equals("q")){
+                keepGoing = false;
+            } else {
+                System.out.println("Please enter a valid option.");
+            }
+        } while(keepGoing);
 
         // Add Profile
 
