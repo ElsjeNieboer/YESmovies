eb09a9e111df3d4cad53eaa6578d8af27a0a4245
Suzanne <suzanne.veenhouwer@hotmail.com>
2019-04-25 15:48:27 +0200

    Add features for creating Profile and adding preferred genres to Profile and Application classes

--- src/nl/YESmovies/Profile.java
+++ src/nl/YESmovies/Profile.java
@@ -3,10 +3,11 @@ package nl.YESmovies;
 import java.util.ArrayList;
 
 public class Profile {
+    public static ArrayList<String> usernameList = new ArrayList<>();
     private long id;
     private String userName;
     private ArrayList<String> watchedMovies;
-    private ArrayList<String> preferredGenres;
+    private int[] preferredGenres = new int[4];
 
     public long getId() {
         return id;
@@ -28,11 +29,40 @@ public class Profile {
         this.watchedMovies = watchedMovies;
     }
 
-    public ArrayList<String> getPreferredGenres() {
-        return preferredGenres;
+    //Traditional getter
+    public int[] getPreferredGenresArray() {
+        return this.preferredGenres;
     }
 
-    public void setPreferredGenres(ArrayList<String> preferredGenres) {
-        this.preferredGenres = preferredGenres;
+    //Getter that displays the preferred genres in text form
+    public String getPreferredGenresText() {
+        String outputPreferredGenres = "";
+        if (this.preferredGenres[0] == 1) {
+            outputPreferredGenres += " Action";
+        }
+        if (this.preferredGenres[1] == 1) {
+            outputPreferredGenres += " Drama";
+        }
+        if (this.preferredGenres[2] == 1) {
+            outputPreferredGenres += " Horror";
+        }
+        if (this.preferredGenres[3] == 1) {
+            outputPreferredGenres += " Fantasy";
+        }
+        return outputPreferredGenres;
+    }
+
+    public void addPreferredGenres(int index) {
+        this.preferredGenres[index] = 1;
+    }
+
+    public void removePreferredGenres(int index) {
+        this.preferredGenres[index] = 0;
+    }
+
+    public Profile(String username) {
+        this.userName = username;
+        usernameList.add(username); //Add username to the ArrayList with all usernames
+        this.id = usernameList.size(); // Generate unique id for created profile
     }
 }
