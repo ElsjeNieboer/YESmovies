a1c1fa5d0f07f6cf846548e839b9b66303fec736
elsjen <elsje_nieboer_1@hotmail.com>
2019-04-30 13:30:00 +0200

    Fix a typo

--- src/nl/YESmovies/Application.java
+++ src/nl/YESmovies/Application.java
@@ -41,56 +41,60 @@ public class Application {
                 Profile newProfile = new Profile(newUsername);
                 Profile.profileObjectList.add(newProfile);
 
-                // adding/removing genres to/from the int[] preferred genres, where index 0 = action, 1 = drama, 2 = horror, and 3 = fantasy
+                // adding/removing genres to/from the int[] preferred genres
                 System.out.println("Thank you for entering a unique username.");
                 boolean retryEnterPreferredGenres = true;
                 do {
-                    System.out.println("Please enter your preferred movie genres to proceed by selecting the numbers indicated in the list below. \n" +
-                            "If you would like to remove genres from your preferences, enter the same number with a '-' in front of it. \n " +
-                            "Selectable genres: 1: Action, 2: Drama, 3: Horror, 4: Fantasy");
+                    System.out.println("Please enter your preferred movie genres to proceed by selecting the numbers indicated in the list below, separated by commas. \n" +
+                            "If you would like to remove genres from your preferences, enter the same number with a '-' in front of it.");
+                    for (int i =0; i < genreOptions.length; i++) {
+                        if ((i+1)%12 == 0) {
+                            System.out.println(genreOptions[i]+"("+(i+1)+")     ");
+                        } else {
+                            System.out.print(genreOptions[i] + "(" + (i + 1) + ")     ");
+                        }
+                    }
+                    System.out.println();
                     String chosenPreferredGenres = reader.nextLine();
-                    if (chosenPreferredGenres.contains("1") && chosenPreferredGenres.indexOf('1') == 0) {
-                        newProfile.addPreferredGenres(0);
-                    } else if (chosenPreferredGenres.contains("1") && chosenPreferredGenres.charAt(chosenPreferredGenres.indexOf('1')-1) != '-') {
-                        newProfile.addPreferredGenres(0);
+
+                    try {
+                        while (chosenPreferredGenres.contains(",")) {
+                            String chosenPreferredGenres_a = chosenPreferredGenres.substring(0, chosenPreferredGenres.indexOf(","));
+                            if (chosenPreferredGenres_a.charAt(0) == '-') {
+                                String chosenPreferredGenres_1 = chosenPreferredGenres_a.substring(1);
+                                newProfile.removePreferredGenres(Integer.parseInt(chosenPreferredGenres_1) - 1);
+                            } else {
+                                newProfile.addPreferredGenres(Integer.parseInt(chosenPreferredGenres_a) - 1);
+                            }
+                            chosenPreferredGenres = chosenPreferredGenres.substring(chosenPreferredGenres.indexOf(",") + 1);
+                        }
+                        if (chosenPreferredGenres.charAt(0) == '-') {
+                            String chosenPreferredGenres_1 = chosenPreferredGenres.substring(1);
+                            newProfile.removePreferredGenres(Integer.parseInt(chosenPreferredGenres_1) - 1);
+                        } else {
+                            newProfile.addPreferredGenres(Integer.parseInt(chosenPreferredGenres) - 1);
+                        }
                     }
-                    if (chosenPreferredGenres.contains("2") && chosenPreferredGenres.indexOf('2') == 0) {
-                        newProfile.addPreferredGenres(1);
-                    } else if (chosenPreferredGenres.contains("2") && chosenPreferredGenres.charAt(chosenPreferredGenres.indexOf('2')-1) != '-') {
-                        newProfile.addPreferredGenres(1);
+                    catch (NumberFormatException nfe) {
+                        System.out.println("You have entered an invalid genre number. Please select a number from the list.");
                     }
-                    if (chosenPreferredGenres.contains("3") && chosenPreferredGenres.indexOf('3') == 0) {
-                        newProfile.addPreferredGenres(2);
-                    } else if (chosenPreferredGenres.contains("3") && chosenPreferredGenres.charAt(chosenPreferredGenres.indexOf('3')-1) != '-') {
-                        newProfile.addPreferredGenres(2);
+                    catch (ArrayIndexOutOfBoundsException aioobe) {
+                        System.out.println("You have entered an invalid genre number. Please select a number from the list.");
                     }
-                    if (chosenPreferredGenres.contains("4") && chosenPreferredGenres.indexOf('4') == 0) {
-                        newProfile.addPreferredGenres(3);
-                    } else if (chosenPreferredGenres.contains("4") && chosenPreferredGenres.charAt(chosenPreferredGenres.indexOf('4')-1) != '-') {
-                        newProfile.addPreferredGenres(3);
-                    }
-                    if (chosenPreferredGenres.contains("-1")) {
-                        newProfile.removePreferredGenres(0);
-                    }
-                    if (chosenPreferredGenres.contains("-2")) {
-                        newProfile.removePreferredGenres(1);
-                    }
-                    if (chosenPreferredGenres.contains("-3")) {
-                        newProfile.removePreferredGenres(2);
-                    }
-                    if (chosenPreferredGenres.contains("-4")) {
-                        newProfile.removePreferredGenres(3);
-                    }
-                    System.out.println("The genres you have chosen are: "+ newProfile.getPreferredGenresText()+". \n " +
-                            "If you are happy with your selection, press 'y'. If you would like to edit your genre selection, press any other key.");
+
+                    System.out.print("The genres you have chosen are: ");
+                    newProfile.getPreferredGenresText();
+                    System.out.println();
+                    System.out.println("If you are happy with your selection, press 'y'. If you would like to edit your genre selection, press any other key.");
                     String changeRetryEnterPreferredGenres = reader.nextLine();
                     if (changeRetryEnterPreferredGenres.equals("y")) {
                         retryEnterPreferredGenres = false;
                     }
                 } while (retryEnterPreferredGenres);
 
-                System.out.println("You have successfully created a new profile with the following username: "+newProfile.getUserName()+".\n" +
-                        "You have entered the following genres as your preferred genres: "+newProfile.getPreferredGenresText()+".");
+                System.out.println("You have successfully created a new profile with the following username: "+newProfile.getUserName()+".");
+                System.out.print("You have entered the following genres as your preferred genres: ");
+                newProfile.getPreferredGenresText();
                 System.out.println("You will now return to the main menu.");
 
             } else if (userInput.equals("m")) {
@@ -171,14 +175,21 @@ public class Application {
                             enteredValidGenre = false;
                         } catch (NumberFormatException e){
 
-                        }
+                        }/* finally {
+                            System.out.println("The chosen genres are "+ movie.getGenreString());
+                            System.out.println("If you are content with the chosen genres, press 'y', if not press any other key.");
+                            userInput = reader.nextLine();
+                            if(!reader.nextLine().equals("y"){
+                                enteredValidGenre = false;
+                            }
+                        }*/
                     } while (!enteredValidGenre);
 
                     Movie.movieObjectList.add(movie);
 
                     System.out.println(movie);
 
-                    System.out.println("If you want to add another movie, press 'y', if not press any key.");
+                    System.out.println("If you want to add another movie, press 'y', if not press any other key.");
 
                     if (!reader.nextLine().equals("y")){
                         addMovie = false;
@@ -252,14 +263,10 @@ public class Application {
                                 }
                                 while(true);
 
-//                                break;
-
                             } else{
                                 System.out.println("Movie not found, please try again.");
                             }
                         }while(true);
-
-//                        break;
 
                     } else {
                         System.out.println("Username not found, please try again.");
@@ -297,11 +304,11 @@ public class Application {
 
                 System.out.println("profileList: " + Arrays.toString(Profile.profileList.toArray()));
 
-                System.out.println("p1 Username: " + p1.getUserName() +
-                        "\np1 Preferred Genres: " + p1.getPreferredGenresText() +
-                        "\np1 Preferred Genres (array): " + p1.getPreferredGenresArray() +
-                        "\np1 Watched movies: " + p1.getWatchedMovies() +
-                        "\np1 Id: " +p1.getId());
+//                System.out.println("p1 Username: " + p1.getUserName() +
+//                        "\np1 Preferred Genres: " + p1.getPreferredGenresText() +
+//                        "\np1 Preferred Genres (array): " + p1.getPreferredGenresArray() +
+//                        "\np1 Watched movies: " + p1.getWatchedMovies() +
+//                        "\np1 Id: " +p1.getId());
 
                 p1.getMyRating("m1");
                 p1.getMyRating("m2");
